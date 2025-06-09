e992694a602f51f279f123debea58245110bb168
adubz510 <adubz510@gmail.com>
2025-06-09 14:20:59 -0700

    start over1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,5 @@ from .restaurant import Restaurant
 from .review import Review
 from .menuItem import MenuItem
 from .image import Image
-from .cart_models import Cart, CartItem
+from .cart import Cart, CartItem
 from .db import environment, SCHEMA
